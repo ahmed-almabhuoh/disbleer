e5dbf9547476d9690ca96bc53bbacfb4294550fc
Ahmed Almabhouh <az540546@gmail.com>
2023-07-25 18:45:11 +0300

    Like Registration Form With Login

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -140,7 +140,7 @@
                                 <!-- You can delete the links only if you purchased the pro version. -->
                                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                                Has no account <a href="{{ route('clientv1.reg') }}">Ceate a new account?</a>
                             </div>
 
                         </div>
