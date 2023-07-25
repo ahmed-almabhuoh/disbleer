a7fb3923f61797c9c1aa005fe0d449bf0f73a73d
Ahmed Almabhouh <az540546@gmail.com>
2023-07-25 19:32:51 +0300

    Fix Icons Bug

--- config/sidebar.php
+++ config/sidebar.php
@@ -13,7 +13,7 @@ return [
     'Account Settings' => [
         'hasSub' => false,
         'subs' => [],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-person-lines-fill"></i>',
         'route' => 'managers.account',
         'permissions' => [],
     ],
@@ -21,7 +21,7 @@ return [
     'Permissions' => [
         'hasSub' => false,
         'subs' => [],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-person-up"></i>',
         'route' => 'permissions',
         'permissions' => [
             'add-permission' => 'form',
@@ -37,7 +37,7 @@ return [
             ['title' => 'Managers', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'managers.index'],
             ['title' => 'Add Manager', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'managers.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-person-bounding-box"></i>',
         'route' => '#',
         'permissions' => [
             'add-manager' => 'form',
@@ -53,7 +53,7 @@ return [
             ['title' => 'Supervisors', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'supervisors.index'],
             ['title' => 'Add Supervisor', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'supervisors.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-person-circle"></i>',
         'route' => '#',
         'permissions' => [
             'add-supervisor' => 'form',
@@ -69,7 +69,7 @@ return [
             ['title' => 'Disables', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'disables.index'],
             ['title' => 'Add Disable', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'disables.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-file-earmark-person"></i>',
         'route' => '#',
         'permissions' => [
             'add-disable' => 'form',
@@ -97,7 +97,7 @@ return [
             ['title' => 'Coursers', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'courses.index'],
             ['title' => 'Add Courser', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'courses.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-journal-album"></i>',
         'route' => '#',
         'permissions' => [
             'add-course' => 'form',
@@ -113,7 +113,7 @@ return [
             ['title' => 'Tests', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'tests.index'],
             ['title' => 'Add Test', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'tests.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-question-square"></i>',
         'route' => '#',
         'permissions' => [
             'add-test' => 'form',
@@ -129,7 +129,7 @@ return [
             ['title' => 'Category', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'categories.index'],
             ['title' => 'Add Category', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'categories.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-bookmarks"></i>',
         'route' => '#',
         'permissions' => [
             'add-category' => 'form',
@@ -145,7 +145,7 @@ return [
             ['title' => 'Roles', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'roles.index'],
             ['title' => 'Add Role', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'roles.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-ui-checks-grid"></i>',
         'route' => '#',
         'permissions' => [
             'add-role' => 'form',
@@ -164,7 +164,7 @@ return [
             ['title' => 'Tags', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'tags.index'],
             ['title' => 'Add Tag', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'tags.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-hash"></i>',
         'route' => '#',
         'permissions' => [
             'add-tag',
@@ -180,7 +180,7 @@ return [
             ['title' => 'Jobs', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'jobs.index'],
             ['title' => 'Add Job', 'icon' => '<i class="bi bi-grid"></i>', 'route' => 'jobs.create'],
         ],
-        'icon' => '<i class="bi bi-grid"></i>',
+        'icon' => '<i class="bi bi-pc-display"></i>',
         'route' => '#',
         'permissions' => [
             'add-job',
