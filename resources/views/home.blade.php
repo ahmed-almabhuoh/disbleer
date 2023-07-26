920bb943d7d9641070e4226989ebf9d92c00d1d5
ِAhmadRaid <ahmadraid256@gmail.com>
2023-07-27 00:28:10 +0300

    show image22

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,7 +46,7 @@
     <div class="container">
       <div class="header-container d-flex align-items-center justify-content-between">
         <div class="logo">
-          <h1 class="text-light"><a href="#"><span>DEnet
+          <h1 class="text-light"><a href="#"><span>DENet
               </span></a></h1>
           <!-- Uncomment below if you prefer to use an image logo -->
           <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
