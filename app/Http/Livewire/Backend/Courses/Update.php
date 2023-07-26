941ff638a93eef91699bd1dc194810fd85a5a271
Ahmed Almabhouh <az540546@gmail.com>
2023-07-26 21:32:32 +0300

    Fix Slug Course Bug

--- app/Http/Livewire/Backend/Courses/Update.php
+++ app/Http/Livewire/Backend/Courses/Update.php
@@ -102,7 +102,7 @@ class Update extends Component
         $course->status = $data['status'];
         $course->category_id = $data['selectedCategory'];
         $course->type = $data['type'];
-        $course->slug = $this->slug;
+        $course->slug = Str::slug($data['name'] . ' ' . time());
         $course->link = $data['courseLink'];
         $course->pre_courses = json_encode($data['pre_courses']);
 
