4dc658071b3b711a049ffa4823ca7ede52c6e667
Ahmed Almabhouh <az540546@gmail.com>
2023-07-26 22:13:19 +0300

    Course Details CSS

--- resources/views/frontend/client-v1/courses/details.blade.php
+++ resources/views/frontend/client-v1/courses/details.blade.php
@@ -42,11 +42,11 @@
                         </div>
 
                         <!-- Controls -->
-                        <a class="carousel-control-prev" href="#courseCarousel" role="button" data-slide="prev">
+                        <a class="carousel-control-prev" style="display: none;" href="#courseCarousel" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only"> {{ __('Previous') }} </span>
                         </a>
-                        <a class="carousel-control-next" href="#courseCarousel" role="button" data-slide="next">
+                        <a class="carousel-control-next" style="display: none;" href="#courseCarousel" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only"> {{ __('Next') }} </span>
                         </a>
